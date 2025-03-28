507684a594af93949ce34532eb65a7c802707e55
phspopos <phspopos@naver.com>
2025-03-28 16:31:24 +0900

    새번째 깃

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,6 +3,9 @@ package test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("잘 되나");
+		System.out.println("11 되나");
+		System.out.println("22잘 되나");
+		System.out.println("33잘 되나");
+		System.out.println("44잘 되나");
 	}
 }
